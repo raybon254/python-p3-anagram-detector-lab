95893f114cbcdf91968ff59814126729a69a2614
Brian <musyimib449@gmail.com>
2025-05-15 00:13:56 +0300

    angram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+        pass
+
+    def match(self,list):
+        new = []
+        for words in list:
+            if sorted(words) == sorted(self.word):
+                new.append(words)
+        return new
+        return []
+                
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
